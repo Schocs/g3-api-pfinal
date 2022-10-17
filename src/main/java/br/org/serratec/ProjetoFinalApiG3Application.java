1b16b2f76377f86f0f10038278d359f3cfb9c95e
João Chocron <schocs13@gmail.com>
2022-10-17 15:44:29 -0300

    apenas a casca do trabalho com os starters iniciais

---
+++ src/main/java/br/org/serratec/ProjetoFinalApiG3Application.java
@@ -0,0 +1,13 @@
+package br.org.serratec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalApiG3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalApiG3Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/org/serratec/ProjetoFinalApiG3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.serratec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalApiG3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
