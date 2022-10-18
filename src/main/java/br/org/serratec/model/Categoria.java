bebec369d77697578b83c95a20bab1372cc0e7a7
Breno Sanglar <brenorick.espindola@gmail.com>
2022-10-18 16:44:06 -0300

    Categoria model

---
+++ src/main/java/br/org/serratec/model/Categoria.java
@@ -0,0 +1,56 @@
+package br.org.serratec.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Categoria {
+
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		@Column(name = "id_categoria")
+		private Long idCategoria;
+		private String nome;
+		private String descricao;
+
+		@OneToMany(mappedBy = "id.categoria", fetch = FetchType.EAGER)
+		private Set<Produto> Produto = new HashSet<>();
+
+		public Long getIdCategoria() {
+			return idCategoria;
+		}
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public String getDescricao() {
+			return descricao;
+		}
+
+		public void setDescricao(String descricao) {
+			this.descricao = descricao;
+		}
+
+		public Set<Produto> getProduto() {
+			return Produto;
+		}
+
+		public void setProduto(Set<Produto> produto) {
+			Produto = produto;
+		}
+		
+		
+}
