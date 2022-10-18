2304978f37f29f8303cfafb0de9279a2bafb39bb
AmandaCasadem <amanda12kcb@gmail.com>
2022-10-17 22:33:56 -0300

    pedido

---
+++ src/main/java/br/org/serratec/model/Pedido.java
@@ -0,0 +1,77 @@
+package br.org.serratec.model;
+
+
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Pedido {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id_pedido")
+	 private Long idPedido;
+	
+	@Column(name="data_pedido")
+	private LocalDateTime dataPedido;
+	
+	@Column (name = "data-entrega")
+	private LocalDate dataEntrega;
+	
+	@Column (name="data_envio")
+	private LocalDateTime dataEnvio;
+	
+	private String status;
+
+	public Long getIdPedido() {
+		return idPedido;
+	}
+
+	public void setIdPedido(Long idPedido) {
+		this.idPedido = idPedido;
+	}
+
+	public LocalDateTime getDataPedido() {
+		return dataPedido;
+	}
+
+	public void setDataPedido(LocalDateTime dataPedido) {
+		this.dataPedido = dataPedido;
+	}
+
+	public LocalDate getDataEntrega() {
+		return dataEntrega;
+	}
+
+	public void setDataEntrega(LocalDate dataEntrega) {
+		this.dataEntrega = dataEntrega;
+	}
+
+	public LocalDateTime getDataEnvio() {
+		return dataEnvio;
+	}
+
+	public void setDataEnvio(LocalDateTime dataEnvio) {
+		this.dataEnvio = dataEnvio;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+	
+
+}
