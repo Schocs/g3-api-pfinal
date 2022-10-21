76adeda73ed5480f8419eadb4144c927abfe92b6
Breno Sanglar <brenorick.espindola@gmail.com>
2022-10-21 16:52:38 -0300

    Erros corrigidos relacionados a Categoria

--- src/main/java/br/org/serratec/controller/CategoriaController.java
+++ src/main/java/br/org/serratec/controller/CategoriaController.java
@@ -1,13 +1,13 @@
 package br.org.serratec.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,63 +15,75 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.org.serratec.model.Categoria;
-import br.org.serratec.repository.CategoriaRepository;
+import br.org.serratec.service.CategoriaService;
 
 @RestController
 @RequestMapping("/categoria")
 public class CategoriaController {
-	@Autowired
-	private CategoriaRepository categoriaRepository;
 
-	@PostMapping("/categoria)")
-	@ResponseStatus(HttpStatus.CREATED)
-	public Categoria inserir(@Valid @RequestBody Categoria categoria) {
-		return categoriaRepository.save(categoria);
+	@Autowired
+	private CategoriaService service;
+
+	public CategoriaController(CategoriaService service) {
+		this.service = service;
 	}
 
-	@PostMapping("/add")
-	public String novo(@Valid Categoria categoria, BindingResult result) {
+	@PostMapping("/cadastrar")
+	public ResponseEntity<Categoria> salvar(@Valid @RequestBody Categoria categoria) {
+		Categoria categoriaSalva = service.salvarCategoria(categoria);
 
-		if (result.hasFieldErrors()) {
-			return "redirect:/form";
+		return new ResponseEntity<>(categoriaSalva, HttpStatus.CREATED);
+	}
+
+	@GetMapping("/listar/{id}")
+	public ResponseEntity<Categoria> getCategoria(@PathVariable Long id) {
+		Optional<Categoria> categoriaExistente = service.encontrarCategoria(id);
+
+		if (categoriaExistente.isPresent()) {
+			return ResponseEntity.ok(categoriaExistente.get());
+
+		} else
+			return ResponseEntity.notFound().build();
+
+	}
+
+	@DeleteMapping("/deletar/{id}")
+	public ResponseEntity<Void> deleteCategoria(@PathVariable Long id) {
+		Optional<Categoria> categoriaExistente = service.encontrarCategoria(id);
+		if (categoriaExistente.isPresent()) {
+			service.deletarCategoria(id);
+			return ResponseEntity.noContent().build();
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@GetMapping("/todos")
+	public ResponseEntity<List<Categoria>> retornaTodos() {
+		List<Categoria> todasCategorias = service.retornaTodasCategorias();
+		System.out.println();
+		if (!todasCategorias.isEmpty()) {
+			return ResponseEntity.ok(todasCategorias);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@PutMapping("/atualizar/{id}")
+	public ResponseEntity<Categoria> atualizar(@PathVariable Long id, @Valid @RequestBody Categoria categoria) {
+
+		Optional<Categoria> categoriaExistente = service.encontrarCategoria(id);
+
+		if (!categoriaExistente.isPresent()) {
+			return ResponseEntity.notFound().build();
 		}
 
-		categoriaRepository.save(categoria);
+		categoria.setIdCategoria(id);
+		service.salvarCategoria(categoria);
 
-		return "redirect:/home";
+		return ResponseEntity.ok(categoriaExistente.get());
 	}
-
-	@GetMapping("/produto/{id}")
-	public List<Categoria> listar() {
-		return categoriaRepository.findAll();
-	}
-
-	@GetMapping(path = { "/{id}" })
-	public ResponseEntity<Categoria> findById(@PathVariable long id) {
-		return categoriaRepository.findById(id).map(record -> ResponseEntity.ok().body(record))
-				.orElse(ResponseEntity.notFound().build());
-	}
-
-	@DeleteMapping(path = { "/{id}" })
-	public ResponseEntity<?> delete(@PathVariable long id) {
-		return categoriaRepository.findById(id).map(record -> {
-			categoriaRepository.deleteById(id);
-			return ResponseEntity.ok().build();
-		}).orElse(ResponseEntity.notFound().build());
-	}
-
-	@PutMapping(value = "/{id}")
-	public ResponseEntity<?> update(@PathVariable("id") long id, @RequestBody Categoria categoria) {
-		return categoriaRepository.findById(id).map(record -> {
-			record.setNome(categoria.getNome());
-			record.setDescricao(categoria.getDescricao());
-			Categoria updated = categoriaRepository.save(record);
-			return ResponseEntity.ok().body(updated);
-		}).orElse(ResponseEntity.notFound().build());
-	}
-
 }
