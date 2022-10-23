0a80bec5d063c7714f82f3b165cd286cd9bf04e2
João Chocron <schocs13@gmail.com>
2022-10-23 14:24:03 -0300

    modificação controllerExceptionHandler

--- src/main/java/br/org/serratec/exception/ControllerExceptionHandler.java
+++ src/main/java/br/org/serratec/exception/ControllerExceptionHandler.java
@@ -16,32 +16,46 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ControllerExceptionHandler extends ResponseEntityExceptionHandler{
-    @ExceptionHandler(EmailException.class)
-    public ResponseEntity<Object> handleEmailException(EmailException ex) {
-        EmailException emailException = new EmailException(ex.getMessage());
-        return ResponseEntity.unprocessableEntity().body(emailException);
-    }
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-            HttpHeaders headers, HttpStatus status, WebRequest request) {
-        List<String> erros = new ArrayList<>();
-        for (FieldError erro : ex.getBindingResult().getFieldErrors()) {
-            erros.add(erro.getField() + ":" + erro.getDefaultMessage());
-        }
-        ErroResposta erroResposta = new ErroResposta(status.value(),
-                "Preenchimento inválido", LocalDateTime.now(), erros);
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
-        return super.handleExceptionInternal(ex, erroResposta, headers, status, request);
-    }
+	@ExceptionHandler(EmailException.class)
+	public ResponseEntity<Object> handleEmailException(EmailException ex) {
+		EmailException emailException = new EmailException(ex.getMessage());
+		return ResponseEntity.unprocessableEntity().body(emailException);
+	}
 
-    @Override
-    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
-            HttpHeaders headers, HttpStatus status, WebRequest request) {
+	@ExceptionHandler(CpfException.class)
+	public ResponseEntity<Object> handleCpfException(CpfException cx) {
+		CpfException cpfException = new CpfException(cx.getMessage());
+		return ResponseEntity.unprocessableEntity().body(cpfException);
+	}
+	
+	@ExceptionHandler(UserException.class)
+	public ResponseEntity<Object> handleUserException(UserException ux) {
+		UserException userException = new UserException(ux.getMessage());
+		return ResponseEntity.unprocessableEntity().body(userException);
+	}
 
-        ErroResposta erroResposta = new ErroResposta(status.value(),
-                "Existem campos inválidos. Confira o preenchimento", LocalDateTime.now(), null);
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		List<String> erros = new ArrayList<>();
+		for (FieldError erro : ex.getBindingResult().getFieldErrors()) {
+			erros.add(erro.getField() + ":" + erro.getDefaultMessage());
+		}
+		ErroResposta erroResposta = new ErroResposta(status.value(), "Preenchimento inválido", LocalDateTime.now(),
+				erros);
 
-        return super.handleExceptionInternal(ex, erroResposta, headers, status, request);
-    }
+		return super.handleExceptionInternal(ex, erroResposta, headers, status, request);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		ErroResposta erroResposta = new ErroResposta(status.value(),
+				"Existem campos inválidos. Confira o preenchimento", LocalDateTime.now(), null);
+
+		return super.handleExceptionInternal(ex, erroResposta, headers, status, request);
+	}
 }
