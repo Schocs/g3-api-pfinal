b313d6f822af07285a8ddbc4f84cd2953a7a80bd
AmandaCasadem <amanda12kcb@gmail.com>
2022-10-23 15:01:43 -0300

    editando os Exceptions

--- src/main/java/br/org/serratec/exception/CpfException.java
+++ src/main/java/br/org/serratec/exception/CpfException.java
@@ -1,7 +1,7 @@
 package br.org.serratec.exception;
 
 public class CpfException extends RuntimeException {
-
+	private static final long serialVersionUID = 1L;
 	public CpfException(String message) {
 		super(message);
 		// TODO Auto-generated constructor stub
